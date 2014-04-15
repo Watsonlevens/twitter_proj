8a59063ddf9fab8202ac68bf55123dcd013aaab1
davirussi <davirussi@gmail.com>
2014-04-15 16:17:42 -0300

    agregador

--- negativos_search.py
+++ negativos_search.py
@@ -92,28 +92,94 @@ def termosnegativos(arq,tipo):
     
     #analisar tweets em busca de termos
     for row in input:
-        write_db('new'+arq, row[:-1]+termos(row.split(',')[0], retipos[tipo]))
+        write_db('neg_'+arq, row[:-1]+termos(row.split(',')[0], retipos[tipo]))
     print 'FINISH'
     return
 
 def removerepetidos_contatermos():
     print "Removendo duplicados..."
     lerarquivo('db_tweettrem','T')
+    lerarquivo('db_tweetauto','C')
+    lerarquivo('db_tweetbicicleta','B')
+    lerarquivo('db_tweetonibus','O')
     return
 
 def loadtermosnegativos():
     try:
         print 'Localizando termos negativos...'
         termosnegativos('db_tweettrem'+'_new.csv', 'T') 
-        #termosnegativos('db_tweetauto'+'_new.csv', 'C')
-        #termosnegativos('db_tweetonibus'+'_new.csv', 'O')
-        #termosnegativos('db_tweetbicicleta'+'_new.csv', 'B')
+        termosnegativos('db_tweetauto'+'_new.csv', 'C')
+        termosnegativos('db_tweetonibus'+'_new.csv', 'O')
+        termosnegativos('db_tweetbicicleta'+'_new.csv', 'B')
     except:
         print'algo errado com os termos negativos'
     return
+
+def agregadordearquivos():
+    ttrem=['t_lotado','t_cheio','t_greve','t_paralisação','t_sem operação','t_não operante','t_problema linha', 't_demora']
+    tauto=['c_trânsito lento','c_obras na faixa','c_sem acesso','c_demora','c_rota inacessível','c_faixa bloqueada','c_problema via',
+            'c_desvio','c_sem sinalização','c_não sinalizado','c_pista interrompida','c_atropelamento','c_buraco']
+    tonibus=['o_não passou','o_lotado','o_cheio','o_trânsito lento','o_obras na faixa','o_defeito elevador','o_sem acesso','o_demora',
+            'o_rota inacessível','o_faixa bloqueada','o_problema via','o_desvio','o_sem sinalização','o_greve','o_paralisação','o_atropelamento',
+            'o_buraco']
+    tbicicleta=['b_sem ciclovia','b_sem ciclofaixa','b_não tem','b_estragado','b_quebrado']
+    dtipos={'T' : ttrem, 'C' : tauto, 'O' : tonibus, 'B' : tbicicleta}
+    
+    ftrem=open('neg_db_tweettrem_new.csv','r')
+    fauto=open('neg_db_tweetauto_new.csv','r')
+    fonibus=open('neg_db_tweetonibus_new.csv', 'r')
+    fbike=open('neg_db_tweetbicicleta_new.csv','r')
+    
+    #fagreg=open('output.csv', 'a')
+    cabecalho = ','.join(ftrem.next().split(',')[0:68]+ttrem+tauto+tonibus+tbicicleta)
+    write_db('output.csv', cabecalho)
+    print cabecalho
+    #print cabecalho.count(',')
+    
+    fauto.next()
+    fonibus.next()
+    fbike.next()
+    
+    tam_t=len(ttrem)
+    tam_a=len(tauto)
+    tam_o=len(tonibus)
+    tam_b=len(tbicicleta)
+    
+    #print ','.join(ftrem.next()[:-1].split(',')[68:]+['0']*(tam_a+tam_o+tam_b))
+    #print ','.join(['0']*tam_t+fauto.next()[:-1].split(',')[68:]+['0']*(tam_o+tam_b))
+    #print ','.join((['0']*(tam_t+tam_a))+fonibus.next()[:-1].split(',')[68:]+['0']*(tam_b))
+    #print ','.join((['0']*(tam_t+tam_a+tam_o))+fbike.next()[:-1].split(',')[68:])
+    
+    print "Juntando arquivos..."
+    print "Trem"
+    for linha in ftrem:
+        content=','.join(linha.split(',')[0:68])+','
+        um_zero=','.join(linha[:-1].split(',')[68:]+['0']*(tam_a+tam_o+tam_b))
+        write_db('output.csv', content+um_zero)
+    
+    print "Auto"
+    for linha in fauto:
+        content=','.join(linha.split(',')[0:68])+','
+        um_zero=','.join(['0']*tam_t+linha[:-1].split(',')[68:]+['0']*(tam_o+tam_b))
+        write_db('output.csv', content+um_zero)
+    
+    print "Onibus"
+    for linha in fonibus:
+        content=','.join(linha.split(',')[0:68])+','
+        um_zero=','.join((['0']*(tam_t+tam_a))+linha[:-1].split(',')[68:]+['0']*(tam_b))
+        write_db('output.csv', content+um_zero)
+    
+    print "Bike"
+    for linha in fbike:
+        content=','.join(linha.split(',')[0:68])+','
+        um_zero=','.join((['0']*(tam_t+tam_a+tam_o))+linha[:-1].split(',')[68:])
+        write_db('output.csv', content+um_zero)
+    return
+
 if __name__ == "__main__":
     # Trem T Auto C Onibus O Bicicleta B
     #removerepetidos_contatermos()
-    loadtermosnegativos()
+    #loadtermosnegativos()
+    agregadordearquivos()
 
 
